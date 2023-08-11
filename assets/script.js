8685d40440f4ad96fd63a4d84086bb116896a410
Hapidr <hapidr24@gmail.com>
2023-08-11 09:49:53 +0700

    Berhasil menambahkan logika pada skor

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,37 @@
 // const button = document.getElementById("button");
 const buttons = document.querySelectorAll("#button");
+const reset = document.getElementById("reset");
+const team_a = document.getElementById("team-a");
+const team_b = document.getElementById("team-b");
+
+const tambah = (object) => {
+  object.innerText++;
+};
+
+const kurang = (object) => {
+  object.innerText--;
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   buttons[0].addEventListener("click", (event) => {
-    console.log(`kurang kiri berhasil`);
+    if (team_a.innerText != 0) {
+      kurang(team_a);
+    }
   });
   buttons[1].addEventListener("click", (event) => {
-    console.log(`tambah kiri berhasil`);
+    tambah(team_a);
   });
   buttons[2].addEventListener("click", (event) => {
-    console.log(`kurang kanan berhasil`);
+    if (team_b.innerText != 0) {
+      kurang(team_b);
+    }
   });
   buttons[3].addEventListener("click", (event) => {
-    console.log(`tambah kanan berhasil`);
+    tambah(team_b);
+  });
+
+  reset.addEventListener("click", () => {
+    team_a.innerText = 0;
+    team_b.innerText = 0;
   });
 });
